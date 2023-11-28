d2aa97645dd6c5ec6c627e86a5448e0043e2cbb9
Mi4nthr0pic <mis4nthr0proton.me>
2023-11-28 17:49:04 -0300

    adding no bloat setup

--- src/adapters/markdown.py
+++ src/adapters/markdown.py
@@ -3,4 +3,5 @@ import markdown
 ERROR_FILE_NOT_FOUND = "Error: File not found"
 
 def build_from_template(path, data):
-    print(path, data)
+    with open('markdown_file.md', 'r', encoding='utf-8') as f:
+    markdown_content = f.read()
